b47adc181c211ac4f1f1ccb33074ded332e9f663
Eyüp ÖZMEN <ozmeneyup60@gmail.com>
2020-04-25 01:11:35 +0300

    Proje dosyası ekle.

---
+++ Nutrition.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Nutrition {
+	enum class DevCountry{USA,CANADA,GERMANY};
+	enum class LessDevCountry{ BRASIL, ARGENTINA, GREECE };
+	friend istream& operator>>(istream&, &);
+public:
+
+
+private:
+	string 
+
+
+};
